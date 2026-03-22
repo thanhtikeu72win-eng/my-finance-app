1a41a4fc965b222f85104b8b61cd4a4d57f0e1cd
thanhtikeu72win-eng <thanhtikeu72win@gmail.com>
2026-03-22 13:23:42 +0630

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
+});
+self.addEventListener('fetch', (e) => {
+    // App ကို Install လုပ်ခွင့်ပြုရန်အတွက် မဖြစ်မနေ လိုအပ်သောလုပ်ဆောင်ချက်
+});
